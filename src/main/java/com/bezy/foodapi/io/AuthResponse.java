8dc94378cc0933b01a31890ce075e9112d294ff8
Bezy427 <rusamubezel@gmail.com>
2026-01-05 20:19:25 +0200

    updated commits

--- src/main/java/com/bezy/foodapi/io/AuthResponse.java
+++ src/main/java/com/bezy/foodapi/io/AuthResponse.java
@@ -1,4 +1,11 @@
 package com.bezy.foodapi.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class AuthResponse {
+    private String email;
+    private String token;
 }
