e3fbf53e454d34fa1c01fc023b4a7e84b8151a30
Bezy427 <rusamubezel@gmail.com>
2026-01-05 20:17:13 +0200

    updated commits

--- src/main/java/com/bezy/foodapi/controller/AuthController.java
+++ src/main/java/com/bezy/foodapi/controller/AuthController.java
@@ -1,4 +1,50 @@
 package com.bezy.foodapi.controller;
 
+import com.bezy.foodapi.io.AuthRequest;
+import com.bezy.foodapi.io.AuthResponse;
+import com.bezy.foodapi.service.AppUserDetailsService;
+import com.bezy.foodapi.util.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final AppUserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthRequest request) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()
+                    )
+            );
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+            String jwtToken = jwtUtil.generateToken(userDetails);
+            return ResponseEntity.ok(new AuthResponse(request.getEmail(), jwtToken));
+
+        } catch (Exception e) {
+            e.printStackTrace();  // <-- SEE REAL ERROR
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid email or password");
+        }
+    }
+
+
 }
--- src/main/java/com/bezy/foodapi/service/AppUserDetailsService.java
+++ src/main/java/com/bezy/foodapi/service/AppUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.bezy.foodapi.service;
 
-public class AppUserDetailsService {
+import com.bezy.foodapi.entity.UserEntity;
+import com.bezy.foodapi.repositories.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@AllArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user =  userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new User(user.getEmail(), user.getPassword(), Collections.emptyList());
+    }
 }
