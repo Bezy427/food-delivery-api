5f94f2149063ee71f8cae1300057a1e040f103d8
Bezy427 <rusamubezel@gmail.com>
2026-01-06 21:19:26 +0200

    updated commits

--- src/main/java/com/bezy/foodapi/repositories/CartRepository.java
+++ src/main/java/com/bezy/foodapi/repositories/CartRepository.java
@@ -1,4 +1,13 @@
 package com.bezy.foodapi.repositories;
 
-public interface CartRepository {
+import com.bezy.foodapi.entity.CartEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface CartRepository extends MongoRepository<CartEntity, String> {
+
+    Optional<CartEntity> findByUserId(String userId);
+
+    void deleteByUserId(String userId);
 }
