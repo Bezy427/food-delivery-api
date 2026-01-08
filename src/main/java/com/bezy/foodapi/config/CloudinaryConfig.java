b565595f2765b14b084fb8459f763009b90da0e7
Bezy427 <rusamubezel@gmail.com>
2026-01-08 11:08:39 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/config/CloudinaryConfig.java
+++ src/main/java/com/bezy/foodapi/config/CloudinaryConfig.java
@@ -1,28 +1,30 @@
 package com.bezy.foodapi.config;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-public class AWSConfig {
-    @Value("${aws.access.key}")
-    private String accessKey;
-    @Value("${aws.secret.key}")
-    private String secretKey;
-    @Value("${aws.region}")
-    private String region;
+public class CloudinaryConfig {
+
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
 
     @Bean
-    public S3Client s3Client(){
-        return S3Client.builder()
-                .region(Region.EU_NORTH_1)
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create("AKIA2QDXLC60TAGIJVGJ", "FI0AE+R8hZTAb2/B1LeEWn5+SaH4H/KbHp8YGyux")))
-                .build();
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
     }
 }
--- src/main/java/com/bezy/foodapi/service/CloudinaryService.java
+++ src/main/java/com/bezy/foodapi/service/CloudinaryService.java
@@ -1,4 +1,22 @@
 package com.bezy.foodapi.service;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@AllArgsConstructor
+@Service
 public class CloudinaryService {
+    private final Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        Map upload = cloudinary.uploader().upload(file.getBytes(),
+                ObjectUtils.asMap("folder", "foodies"));
+        return upload.get("secure_url").toString();
+    }
 }
