30db2258190f5ad6a1a26c89b3c33c506c46b15d
Bezy427 <rusamubezel@gmail.com>
2026-01-08 11:09:28 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/FoodapiApplication.java
+++ src/main/java/com/bezy/foodapi/FoodapiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoodapiApplication.class, args);
+    }
 
 }
