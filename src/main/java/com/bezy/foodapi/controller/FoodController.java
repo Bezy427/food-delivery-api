05556e927ab2a0a5cd2d99088e8a708d89e58643
Bezy427 <rusamubezel@gmail.com>
2026-01-08 11:09:52 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/controller/FoodController.java
+++ src/main/java/com/bezy/foodapi/controller/FoodController.java
@@ -1,4 +1,63 @@
 package com.bezy.foodapi.controller;
 
+import com.bezy.foodapi.io.FoodRequest;
+import com.bezy.foodapi.io.FoodResponse;
+import com.bezy.foodapi.service.FoodService;
+import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/foods")
+@CrossOrigin("*")
 public class FoodController {
+    private final FoodService foodService;
+
+    @PostMapping
+    public FoodResponse addFood(@RequestPart("food") String foodString,
+                                          @RequestPart("file")MultipartFile file
+    ){
+        ObjectMapper mapper = new ObjectMapper();
+        FoodRequest request = null;
+        try {
+            request = mapper.readValue(foodString, FoodRequest.class);
+        } catch (IOException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Invalid JSON format"
+            );
+        }
+        return foodService.addFood(request, file);
+    }
+
+    @GetMapping
+    public List<FoodResponse> readFoods(){
+
+        return foodService.readFoods();
+    }
+
+    @GetMapping("/{id}")
+    public FoodResponse readFood(
+            @PathVariable String id
+    ){
+        return foodService.readFood(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteFood(
+            @PathVariable String id
+    ){
+        foodService.deleteFood(id);
+    }
+
 }
--- src/main/java/com/bezy/foodapi/entity/FoodEntity.java
+++ src/main/java/com/bezy/foodapi/entity/FoodEntity.java
@@ -1,4 +1,24 @@
 package com.bezy.foodapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.TextIndexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection="foods")
 public class FoodEntity {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String imageUrl;
+    private double price;
+    private String category;
 }
