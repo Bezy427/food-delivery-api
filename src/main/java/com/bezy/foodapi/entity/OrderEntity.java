2b1a610d6e192eb8223a3175f2cd5d804ab254ef
Bezy427 <rusamubezel@gmail.com>
2026-01-09 21:16:52 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/entity/OrderEntity.java
+++ src/main/java/com/bezy/foodapi/entity/OrderEntity.java
@@ -1,4 +1,33 @@
 package com.bezy.foodapi.entity;
 
+import com.bezy.foodapi.io.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Document(collection="orders")
 public class OrderEntity {
+    @Id
+    private String id;
+    private String userId;
+    private String userAddress;
+    private String phoneNumber;
+    private String email;
+    private List<OrderItem> orderItems;
+    private double amount;
+    private String payPalOrderId;
+    private String paymentStatus;
+    private String payPalSignature;
+    private String payPalPaymentId;
+    private String status;
+
 }
