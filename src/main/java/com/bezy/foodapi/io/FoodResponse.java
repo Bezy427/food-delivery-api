830a60f2e8a19f422e5ade95e1247d647ba90afc
Bezy427 <rusamubezel@gmail.com>
2026-01-09 21:15:15 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/io/FoodResponse.java
+++ src/main/java/com/bezy/foodapi/io/FoodResponse.java
@@ -1,4 +1,21 @@
 package com.bezy.foodapi.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FoodResponse {
+    private String id;
+    private String name;
+    private String description;
+    private String imageUrl;
+
+    private double price;
+    private String category;
+
 }
--- src/main/java/com/bezy/foodapi/service/FoodService.java
+++ src/main/java/com/bezy/foodapi/service/FoodService.java
@@ -1,4 +1,21 @@
 package com.bezy.foodapi.service;
 
+import com.bezy.foodapi.io.FoodRequest;
+import com.bezy.foodapi.io.FoodResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface FoodService {
+
+
+    String uploadFile(MultipartFile file);
+
+    FoodResponse addFood(FoodRequest request, MultipartFile file);
+
+    List<FoodResponse> readFoods();
+
+    FoodResponse readFood(String id);
+
+    void deleteFood(String id);
 }
