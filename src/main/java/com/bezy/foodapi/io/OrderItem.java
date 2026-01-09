a98019244e83c034c676e8eed5b5f2eebe6dac64
Bezy427 <rusamubezel@gmail.com>
2026-01-09 21:19:09 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/io/OrderItem.java
+++ src/main/java/com/bezy/foodapi/io/OrderItem.java
@@ -1,4 +1,20 @@
 package com.bezy.foodapi.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class OrderItem {
+    private String foodId;
+    private int quantity;
+    private double price;
+    private String category;
+    private String imageUrl;
+    private String description;
+    private String name;
 }
