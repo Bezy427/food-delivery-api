1284694e063f7c45f7332bb29b37a8536670a724
Bezy427 <rusamubezel@gmail.com>
2026-01-11 00:47:42 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/controller/UserController.java
+++ src/main/java/com/bezy/foodapi/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.bezy.foodapi.controller;
 
+import com.bezy.foodapi.io.UserRequest;
+import com.bezy.foodapi.io.UserResponse;
+import com.bezy.foodapi.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponse register(@RequestBody UserRequest request) {
+        return userService.registerUser(request);
+    }
 }
