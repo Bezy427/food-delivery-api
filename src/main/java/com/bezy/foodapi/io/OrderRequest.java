67fe3489a47ba64c910f045aa55239187b87153a
Bezy427 <rusamubezel@gmail.com>
2026-01-10 14:00:53 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/io/OrderRequest.java
+++ src/main/java/com/bezy/foodapi/io/OrderRequest.java
@@ -1,4 +1,20 @@
 package com.bezy.foodapi.io;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class OrderRequest {
+    private String userId;
+    private List<OrderItem> orderItems;
+    private String userAddress;
+    private double amount;
 }
--- src/main/java/com/bezy/foodapi/io/OrderResponse.java
+++ src/main/java/com/bezy/foodapi/io/OrderResponse.java
@@ -1,4 +1,24 @@
 package com.bezy.foodapi.io;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class OrderResponse {
+    private String id;
+    private String userId;
+    private String orderId;
+    private String paymentApprovalUrl;
+    private String userAddress;
+    private String phoneNumber;
+    private String email;
+    private double amount;
+    private String paymentStatus;
+    private String paypalOrderId;
+    private String paypalSignature;
+    private String orderStatus;
+    private List<OrderItem> orderItems;
 }
