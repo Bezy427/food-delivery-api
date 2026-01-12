569f63032d389a05d0e6e73bd2220800c0abfc7f
Bezy427 <rusamubezel@gmail.com>
2026-01-12 23:58:44 +0200

    initial commits

--- src/main/java/com/bezy/foodapi/io/UserResponse.java
+++ src/main/java/com/bezy/foodapi/io/UserResponse.java
@@ -1,4 +1,16 @@
 package com.bezy.foodapi.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
+    private String id;
+    private String name;
+    private String email;
 }
